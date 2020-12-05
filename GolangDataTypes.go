2254f9bd1462f685a3e8f525d5b7051297902942
Eevee8869 <49135436+Eevee8869@users.noreply.github.com>
2020-12-05 11:16:52 +0530

    Create GolangDataTypes.go

---
+++ GolangDataTypes.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var i int //int = integer 
+	var f float64 //float64 = float value like 3.141592653589793
+	var b bool //bool holds 2 values only true or false that's it
+	var s string //a string is traditionally a sequence of characters for ex "ThisIsAString8292" 
+	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+}
+//oh yea btw // = single line comment and
+// /* */ = multiline comment
+/* multiline 
+cmt
+*/
